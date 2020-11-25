1fbb46d9d7e47d24644999f05104954d8b40c2e9
khangtran <kt36@njit.edu>
2020-11-25 15:15:30 -0500

    Feature: API

--- app/app.py
+++ app/app.py
@@ -100,19 +100,38 @@ def api_retrieve(city_id) -> str:
 
 @app.route('/api/v1/oscar/', methods=['POST'])
 def api_add() -> str:
+    content = request.json
+    cursor = mysql.get_db().cursor()
+    inputData = (content['id'], content['year'], content['age'],
+                 content['name'], content['movie'])
+    sql_insert_query = """INSERT INTO oscarAgeFemale (id,`year`,age,`name`,movie) VALUES (%s, %s,%s, %s,%s) """
+    cursor.execute(sql_insert_query, inputData)
+    mysql.get_db().commit()
     resp = Response(status=201, mimetype='application/json')
     return resp
 
 
 @app.route('/api/v1/oscar/<int:city_id>', methods=['PUT'])
 def api_edit(city_id) -> str:
-    resp = Response(status=201, mimetype='application/json')
+    cursor = mysql.get_db().cursor()
+    content = request.json
+    inputData = (content['year'], content['age'], content['name'],
+                 content['movie'], city_id)
+    sql_update_query = """UPDATE oscarAgeFemale t SET t.year = %s, t.age = %s, t.name = %s, t.movie = 
+            %s WHERE t.id = %s """
+    cursor.execute(sql_update_query, inputData)
+    mysql.get_db().commit()
+    resp = Response(status=200, mimetype='application/json')
     return resp
 
 
-@app.route('/api/oscar/<int:city_id>', methods=['DELETE'])
+@app.route('/api/v1/oscar/<int:city_id>', methods=['DELETE'])
 def api_delete(city_id) -> str:
-    resp = Response(status=210, mimetype='application/json')
+    cursor = mysql.get_db().cursor()
+    sql_delete_query = """DELETE FROM oscarAgeFemale WHERE id = %s """
+    cursor.execute(sql_delete_query, city_id)
+    mysql.get_db().commit()
+    resp = Response(status=200, mimetype='application/json')
     return resp
 
 
